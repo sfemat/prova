25965c5d73497e27a8226cc5dad08fa38a1048f7
sfemat <sfemat@libero.it>
2017-10-09 11:57:42 +0200

    qualcosa

---
+++ choose.js
@@ -0,0 +1,4 @@
+var nome=prompt("Inserisci il tuo nome");
+var colore=prompt("Qual'e' il tuo colore preferito tra yellow, red and blue?");
+var lingua=prompt("In che lingua vuoi visualizzare la pagina? IT/ENG");
+document.body.style.background = colore;
