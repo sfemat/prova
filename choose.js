5ceec6767c8535ad7d8155707dbc14e9d3f412e5
sfemat <sfemat@libero.it>
2017-10-14 17:51:37 +0200

    aggiungo prova2.txt

--- choose.js
+++
@@ -1,4 +0,0 @@
-var nome=prompt("Inserisci il tuo nome");
-var colore=prompt("Qual'e' il tuo colore preferito tra yellow, red and blue?");
-var lingua=prompt("In che lingua vuoi visualizzare la pagina? IT/ENG");
-document.body.style.background = colore;
