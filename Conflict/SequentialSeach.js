3ec1218d2573adf54c5a9e328c3d534836a546a7
sfemat <sfemat@libero.it>
2017-10-14 18:12:47 +0200

    modifiche locali

--- Conflict/SequentialSeach.js
+++ Conflict/SequentialSeach.js
@@ -1,6 +1,6 @@
 public class SequentialSearch {
         public static boolean contains(int[] a, int b){
-                for (int i = 0; i<a.length ; i++) {
+                for (int i : a) {
                         if (a[i]==b){
                                 return true;
                         }
