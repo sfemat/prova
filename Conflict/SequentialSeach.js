941e0a6aa5aa161abf54d704c11fea65d3ee984f
sfemat <32337779+sfemat@users.noreply.github.com>
2017-10-14 18:11:44 +0200

    Update SequentialSeach.js

--- Conflict/SequentialSeach.js
+++ Conflict/SequentialSeach.js
@@ -1,5 +1,5 @@
 public class SequentialSearch {
-        public static boolean contains(int[] a, int b){
+        public static boolean contains(long[] a, long b){
                 for (int i = 0; i<a.length ; i++) {
                         if (a[i]==b){
                                 return true;
